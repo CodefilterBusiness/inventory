da6b4cdca930446bb8ea3f7f12ee850a37736028
pafbmsSupply <pafbms.arm@gmail.com>
2024-07-14 21:20:46 +0800

    added outbound

--- inventory/inventory/settings.py
+++ inventory/inventory/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # THIRD PARTY
-    'suit',
+
     # APPS
     'stocks',
     'outbound',
--- inventory/outbound/admin.py
+++ inventory/outbound/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
+from django.http import HttpResponse
+import csv
+from django.utils.translation import gettext_lazy as _
+from django.db.models import Sum
+from .models import Outbound, OutboundItem
 
-# Register your models here.
+class OutboundItemInline(admin.TabularInline):
+    model = OutboundItem
+    extra = 1
+
+class OutboundAdmin(admin.ModelAdmin):
+    list_display = ('transaction_ref', 'total_quantity', 'outbound_date', 'processed_by')
+    search_fields = ('transaction_ref', 'processed_by__username')
+    readonly_fields = ('transaction_ref', 'unit', 'get_items_list')  # Adjusted readonly_fields
+
+    def get_queryset(self, request):
+        # Override the queryset to annotate with total quantity
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(total_quantity=Sum('outbounditem__quantity'))
+        return queryset
+
+    def total_quantity(self, obj):
+        # Display the annotated total quantity
+        return obj.total_quantity
+
+    def unit(self, obj):
+        # Display the unit of the Stock associated with this Outbound instance
+        return obj.items.first().stock.unit if obj.items.exists() else ''
+
+    def get_items_list(self, obj):
+        # Retrieve and format the list of items associated with this Outbound instance
+        items_list = obj.outbounditem_set.all()
+        return ', '.join([f'{item.stock.stock_no} ({item.quantity})' for item in items_list])
+
+    def export_as_csv(self, request, queryset):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="outbound.csv"'
+
+        writer = csv.writer(response)
+        writer.writerow(['Transaction Reference', 'Outbound Date', 'Processed By', 'Total Quantity', 'Unit', 'Items'])
+
+        for obj in queryset:
+            items_list = obj.outbounditem_set.all()
+            writer.writerow(
+                [obj.transaction_ref, obj.outbound_date, obj.processed_by, obj.total_quantity, obj.unit, items_list])
+
+        return response
+
+    export_as_csv.short_description = _('Export selected Outbounds as CSV')
+
+    total_quantity.short_description = 'Total Quantity'  # Set the column header
+    total_quantity.admin_order_field = 'total_quantity'  # Enable ordering by this column
+
+    unit.short_description = 'Unit'
+    get_items_list.short_description = 'Items'
+
+# Register your models with the admin site
+admin.site.register(Outbound, OutboundAdmin)
