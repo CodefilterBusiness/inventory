71275962a19506abce5d460981f450297d1efed7
pafbmsSupply <pafbms.arm@gmail.com>
2024-07-14 21:32:27 +0800

    outbound

--- inventory/outbound/admin.py
+++ inventory/outbound/admin.py
@@ -9,10 +9,23 @@ class OutboundItemInline(admin.TabularInline):
     model = OutboundItem
     extra = 1
 
+
 class OutboundAdmin(admin.ModelAdmin):
     list_display = ('transaction_ref', 'total_quantity', 'outbound_date', 'processed_by')
     search_fields = ('transaction_ref', 'processed_by__username')
     readonly_fields = ('transaction_ref', 'unit', 'get_items_list')  # Adjusted readonly_fields
+
+    inlines = [OutboundItemInline]  # Include inline form for OutboundItem
+
+    fieldsets = (
+        (None, {
+            'fields': ('outbound_date', 'processed_by', 'transaction_ref')
+        }),
+        ('Unit and Items', {
+            'fields': ('unit',),
+            'classes': ('collapse',)  # Optional: Hide by default
+        }),
+    )
 
     def get_queryset(self, request):
         # Override the queryset to annotate with total quantity
@@ -25,8 +38,9 @@ class OutboundAdmin(admin.ModelAdmin):
         return obj.total_quantity
 
     def unit(self, obj):
-        # Display the unit of the Stock associated with this Outbound instance
-        return obj.items.first().stock.unit if obj.items.exists() else ''
+        # Display the unit of the first Stock associated with this Outbound instance
+        first_item = obj.outbounditem_set.first()
+        return first_item.stock.unit if first_item else ''
 
     def get_items_list(self, obj):
         # Retrieve and format the list of items associated with this Outbound instance
@@ -54,6 +68,13 @@ class OutboundAdmin(admin.ModelAdmin):
 
     unit.short_description = 'Unit'
     get_items_list.short_description = 'Items'
+
+    # Override save_related to update unit based on first item's stock
+    def save_related(self, request, form, formsets, change):
+        super().save_related(request, form, formsets, change)
+        obj = form.instance
+        obj.unit = obj.items.first().stock.unit if obj.items.exists() else ''
+        obj.save()
 
 # Register your models with the admin site
 admin.site.register(Outbound, OutboundAdmin)
