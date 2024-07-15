f47257ea5ee700dff881e5e620d36c2fadf86bbc
kinaadman <kinaadman2024bravo@gmail.com>
2024-07-15 12:35:32 +0800

    changes

--- inventory/outbound/admin.py
+++ inventory/outbound/admin.py
@@ -12,8 +12,8 @@ class OutboundItemInline(admin.TabularInline):
 
 class OutboundAdmin(admin.ModelAdmin):
     list_display = ('transaction_ref', 'total_quantity', 'outbound_date', 'processed_by')
-    search_fields = ('transaction_ref', 'processed_by__username')
-    readonly_fields = ('transaction_ref', 'unit', 'get_items_list')  # Adjusted readonly_fields
+    search_fields = ('transaction_r ef', 'processed_by__username')
+    readonly_fields = ('transaction_ref', 'get_items_list')  # Adjusted readonly_fields
 
     inlines = [OutboundItemInline]  # Include inline form for OutboundItem
 
@@ -21,10 +21,10 @@ class OutboundAdmin(admin.ModelAdmin):
         (None, {
             'fields': ('outbound_date', 'processed_by', 'transaction_ref')
         }),
-        ('Unit and Items', {
-            'fields': ('unit',),
-            'classes': ('collapse',)  # Optional: Hide by default
-        }),
+        # ('Unit and Items', {
+        #     'fields': ('unit',),
+        #     'classes': ('collapse',)  # Optional: Hide by default
+        # }),
     )
 
     def get_queryset(self, request):
@@ -37,10 +37,27 @@ class OutboundAdmin(admin.ModelAdmin):
         # Display the annotated total quantity
         return obj.total_quantity
 
-    def unit(self, obj):
-        # Display the unit of the first Stock associated with this Outbound instance
-        first_item = obj.outbounditem_set.first()
-        return first_item.stock.unit if first_item else ''
+    # def unit(self, obj):
+    #     return obj.stock.unit if obj.stock else ''
+
+    #     # Override formfield_for_foreignkey to customize the unit field
+    #
+    # def formfield_for_foreignkey(self, db_field, request, **kwargs):
+    #     if db_field.name == 'stock':  # Adjust based on your actual foreign key field name
+    #         kwargs['queryset'] = Stock.objects.all()  # Replace with your Stock queryset
+    #     return super().formfield_for_foreignkey(db_field, request, **kwargs)
+    #
+    #     # Override save_related to handle saving 'unit' per item
+    #
+    # def save_related(self, request, form, formsets, change):
+    #     super().save_related(request, form, formsets, change)
+    #     obj = form.instance
+    #     for formset in formsets:
+    #         if isinstance(formset, OutboundItemInline):
+    #             instances = formset.save(commit=False)
+    #             for instance in instances:
+    #                 instance.unit = instance.stock.unit if instance.stock else ''
+    #                 instance.save()
 
     def get_items_list(self, obj):
         # Retrieve and format the list of items associated with this Outbound instance
@@ -66,14 +83,14 @@ class OutboundAdmin(admin.ModelAdmin):
     total_quantity.short_description = 'Total Quantity'  # Set the column header
     total_quantity.admin_order_field = 'total_quantity'  # Enable ordering by this column
 
-    unit.short_description = 'Unit'
+    # unit.short_description = 'Unit'
     get_items_list.short_description = 'Items'
 
     # Override save_related to update unit based on first item's stock
     def save_related(self, request, form, formsets, change):
         super().save_related(request, form, formsets, change)
         obj = form.instance
-        obj.unit = obj.items.first().stock.unit if obj.items.exists() else ''
+        obj.unit = obj.items.first().unit if obj.items.exists() else ''
         obj.save()
 
 # Register your models with the admin site
